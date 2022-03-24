8291125ac055d7cb71de907f034a2efc14d91482
Julio Moreno <julio.morenoshz@uanl.edu.mx>
2022-03-24 16:01:17 -0600

    delete not used branches (not the right way but pull them master*)

--- WorkingArea.py
+++ WorkingArea.py
@@ -10,15 +10,15 @@ class Tab2Widgets:
     def __init__(self):
         pass
 
-    def CreateWidgets_Tab2(self,ParentFrame):
-        self.CreateLabels(ParentFrame)
+    def CreateWidgets_Tab2(self,ParentFrame):    
         self.CreateLabelFrames(ParentFrame)
+        self.CreateLabels(ParentFrame)
         self.CreateButtons(ParentFrame)
     
     
     def CreateLabels(self,ParentFrame):
         #Tab Title
-        Label(
+        self.Title=Label(
             ParentFrame,
             text = 'Welcome "WorkingArea"',
             padx=50,pady=20,
@@ -27,17 +27,21 @@ class Tab2Widgets:
             relief="solid"
             ).grid(row=0, column=0, columnspan=4)
 
+
     def CreateLabelFrames(self,ParentFrame):
-        self.MeasurementTime = LabelFrame(
-            ParentFrame, 
-            width=300, 
-            height= 200, 
-            text="Tiempo de Medición",
-            font=('Arial Rounded MT Bold', 12),
-            labelanchor= "nw",
+        self.MeasurementTime = LabelFrame(ParentFrame, width=300, height= 200, text="Tiempo de Medición", 
+        font=('Arial Rounded MT Bold', 12), labelanchor= "nw", relief="solid").grid(row=1, column=0, pady=20)
+
+        # Labels in MeasurementTime Label Frame
+        Label(
+            ParentFrame,
+            text = 'Tiempo Inicial',
+            #padx=50,pady=20,
+            anchor='e',
+            font=('Arial Rounded MT Bold', 9),
             relief="solid"
-            ).grid(row=1, column=0, pady=20)
-        pass
+            ).grid(row=3, column=0)
+        
 
     def CreateButtons(self,ParentFrame):
         pass
