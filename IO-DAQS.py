b1596633e9cda5b3e534bf147e61043546aa4633
Julio Moreno <julio.morenoshz@uanl.edu.mx>
2022-03-25 22:41:55 -0600

    Signal section created!, missing start and stop buttons

--- IO-DAQS.py
+++ IO-DAQS.py
@@ -13,7 +13,7 @@ class IO_DAQS(Tab2Widgets):
         super().__init__()
         self.MainWindow = Window
         self.MainWindow_Width="1200"
-        self.MainWindow_Height="550"
+        self.MainWindow_Height="600"
         self.MainWindow.geometry('{}x{}'.format(self.MainWindow_Width,self.MainWindow_Height))
         
         self.CreateWidgets()
@@ -40,14 +40,14 @@ class IO_DAQS(Tab2Widgets):
             self.FillTab3(ParentName)
     #To do: Filltab1 and FilTab3 muest be gone!     
     def FillTab1(self,ParentName):
-        self.FrameTab1 = ttk.Frame(ParentName, width='1200', height='550')
+        self.FrameTab1 = ttk.Frame(ParentName, width='1200', height='600')
         self.FrameTab1.grid(row=1,column=0)
         
         self.Title1 = Label(self.FrameTab1, text = 'Welcome "Home" ',padx=50, pady=20,font=('Arial Rounded MT Bold', 20))
         self.Title1.grid(row=0, column=0)
     
     def FillTab3(self,ParentName):
-        self.FrameTab3 = ttk.Frame(ParentName, width='1200', height='550')
+        self.FrameTab3 = ttk.Frame(ParentName, width='1200', height='600')
         self.FrameTab3.grid(row=1,column=0)
 
         self.Title3 = Label(self.FrameTab3, text = 'Welcome "About us"',padx=50,pady=20, font=('Arial Rounded MT Bold', 20))
--- WorkingArea.py
+++ WorkingArea.py
@@ -16,26 +16,33 @@ class Tab2Widgets:
         self.CreateLabelFrames(self.FrameTab2)
         self.CreateLabels(self.FrameTab2)
         self.CreateSpinBox(self.MeasurementTime_LFrame)
-        self.CreateButtons()
-    
+        self.CreateComboboxes()
+        self.CreateSliders()
+        self.CreateRadiobuttons()
         return self.FrameTab2
-    def CreateButtons(self):
+
+
+
+    def CreatButtons(self):
+        pass
+
+
+
+    def CreateComboboxes(self):
+        #-------Comboboxes in SamplingRatio Label Frame----------#
         self.SamplingUnit = ttk.Combobox(self.SamplingRatio_LFrame, value=['Tiempo','Frecuencia'])
         self.SamplingUnit.set('Tiempo')
         self.SamplingUnit.grid(row=0, column=1)
 
         self.SamplingPrefix = ttk.Combobox(self.SamplingRatio_LFrame, value=['ks','ms','us','ps'])
         self.SamplingPrefix.set('s')
-        self.SamplingPrefix.grid(row=1, column=1)
-        pass   
-
+        self.SamplingPrefix.grid(row=1, column=1)  
 
 
 
     def CreateFrames(self,ParentName):
-        self.FrameTab2 = ttk.Frame(ParentName, width='1200', height='550')
+        self.FrameTab2 = ttk.Frame(ParentName, width='1200', height='600')
         self.FrameTab2.grid(row=1,column=0)
-
 
 
 
@@ -58,18 +65,52 @@ class Tab2Widgets:
         self.SamplingUnitLabel=Label(self.SamplingRatio_LFrame, text = 'Unidad de Muestreo:  ', anchor='c', font=('Arial Rounded MT Bold', 9), relief="solid")
         self.SamplingUnitLabel.grid(row=0, column=0, ipadx=1, ipady=5, pady=5)
 
+        self.UnitPrefixLabel=Label(self.SamplingRatio_LFrame, text = 'Prefijo de Unidad:  ', anchor='c', font=('Arial Rounded MT Bold', 9), relief="solid")
+        self.UnitPrefixLabel.grid(row=1, column=0, ipadx=1, ipady=5, pady=5)
+
+        self.MagnitudLabel=Label(self.SamplingRatio_LFrame, text = 'Prefijo de Unidad:  ', anchor='c', font=('Arial Rounded MT Bold', 9), relief="solid")
+        self.MagnitudLabel.grid(row=2, column=0, ipadx=1, ipady=5, pady=5)
+
+        #-------Labels in Signal Label Frame-----------------#
+        self.SignalTypeLabel=Label(self.Signal_LFrame, text = 'Tipo de Señal:  ', anchor='c', font=('Arial Rounded MT Bold', 9), relief="solid")
+        self.SignalTypeLabel.grid(row=0, column=0, ipadx=1, ipady=5, pady=5, columnspan=2)
 
 
 
     def CreateLabelFrames(self,ParentFrame):
         self.MeasurementTime_LFrame = LabelFrame(ParentFrame, width=300, height= 200, text="Tiempo de Medición", 
-        font=('Arial Rounded MT Bold', 12), labelanchor= "n", relief="solid")
+        font=('Arial Rounded MT Bold', 10), labelanchor= "n", relief="solid")
         self.MeasurementTime_LFrame.grid(row=1, column=0, padx = 40, pady=5, ipadx=20,ipady=1)
 
         self.SamplingRatio_LFrame = LabelFrame(ParentFrame, width=300, height= 200, text="Razón de Muestreo", 
-        font=('Arial Rounded MT Bold', 12), labelanchor= "n", relief="solid")
-        self.SamplingRatio_LFrame.grid(row=2, column=0, padx = 40, pady=5, ipadx=20,ipady=5)
+        font=('Arial Rounded MT Bold', 10), labelanchor= "n", relief="solid")
+        self.SamplingRatio_LFrame.grid(row=1, column=1, padx = 40, pady=5, ipadx=20,ipady=5)
 
+        self.Signal_LFrame = LabelFrame(ParentFrame, width=300, height= 200, text="Careacterísticas de señal", 
+        font=('Arial Rounded MT Bold', 10), labelanchor= "n", relief="solid")
+        self.Signal_LFrame.grid(row=1, column=2, padx = 40, pady=5, ipadx=20,ipady=5)
+
+
+
+    def CreateRadiobuttons(self):
+        #-------Radiobuttons in Signal Label Frame----------#
+        self.StepInput = Radiobutton(self.Signal_LFrame, text='Escalon', value='step')
+        self.StepInput.grid(row=1, column=0 )
+
+        self.SlopeInput = Radiobutton(self.Signal_LFrame, text='Rampa', value='slope')
+        self.SlopeInput.grid(row=1, column=1)
+
+        self.NoiseInput = Radiobutton(self.Signal_LFrame, text='Ruido', value='noise')
+        self.NoiseInput.grid(row=2, column=0)
+
+        self.SineInput = Radiobutton(self.Signal_LFrame, text='Senoidal', value='senoidal')
+        self.SineInput.grid(row=2, column=1)
+        
+
+
+    def CreateSliders(self):
+        self.InputVoltage = Scale(self.Signal_LFrame, from_=-10, to=10, orient='horizontal', resolution=.001, label='Voltaje')
+        self.InputVoltage.grid(row=3, column=0, columnspan=2)
 
 
 
@@ -79,9 +120,9 @@ class Tab2Widgets:
         self.StartTime.grid(row=0, column=1, ipadx=1,ipady=5, padx=8 )
 
         self.EndTime = Spinbox(ParentFrame, from_=0.0, to=1800.0)
-        self.EndTime.grid(row=1, column=1, ipadx=1,ipady=5, padx=8)
+        self.EndTime.grid(row=1, column=1, ipadx=0,ipady=0, padx=8)
 
         #-------SpinBoxes in SamplingRatio Label Frame----------#
         self.SampligCoefficient = Spinbox(self.SamplingRatio_LFrame, from_=0.0, to=1800.0)
-        self.SampligCoefficient.grid(row=1, column=0, ipadx=1,ipady=5, padx=8)
+        self.SampligCoefficient.grid(row=2, column=1, ipadx=0,ipady=0, padx=8)
 
