62cce918c3ba8a8cdff197144af5ad2017097d57
Julio Moreno <julio.morenoshz@uanl.edu.mx>
2022-04-20 01:06:25 -0500

    Arduino serial info decoding succesful!

--- myserial/myserial.ino
+++ myserial/myserial.ino
@@ -1,28 +1,45 @@
-int  PIN_led = 2;
-String command,pin_name,pin_val;
-int led_val = 0;
-int cut_car; 
+int  PIN_led = 13;
+
+#define ArraySize 4
+String parameters[ArraySize], DataChain, element;
+#define SeparatorCharacter ","
+int InitialPosition, Separator_LastPosition, ArrayIndex;
 
 void setup() {
   Serial.begin(9600);                     //Serial Port opened at a baudrate of 9600 b/s
   pinMode(PIN_led,OUTPUT);                //The pin "2" is named as "señal" and is setted up as an output
-  digitalWrite(PIN_led,led_val);                // The initial state of "señal" is off
-  
+  digitalWrite(PIN_led,1);                // The initial state of "señal" is off
 }
 
 void loop() {
-  if(Serial.available()){               //Check if the serial port sends for something (a command)
-    command = Serial.readString();      // Command format: (led,x) -----> the first parameter indicates the name of the output, and second its value
-    cut_car = command.indexOf(",");
-    pin_name = command.substring(0,cut_car);
-    pin_val = command.substring(cut_car+1);
-
-    if(led_val != pin_val.toInt()){
-      led_val = pin_val.toInt();
-      digitalWrite(PIN_led,led_val);
-    }
-    Serial.print(pin_name +" state:"+pin_val);
-    Serial.print(" (1 for high, 0 for low)");
-    Serial.println(" ");
-  }
+  DecodeDataChain();
  }
+
+ void DecodeDataChain() {
+  if(Serial.available()){
+    DataChain = Serial.readString();
+    Serial.println(DataChain);
+    InitialPosition=0;
+    Separator_LastPosition = DataChain.indexOf(SeparatorCharacter,InitialPosition);
+    
+    ArrayIndex = 0;
+    while (Separator_LastPosition!=-1) {
+      Serial.print(ArrayIndex);
+      element = DataChain.substring(InitialPosition,  Separator_LastPosition); 
+      parameters[ArrayIndex]= element;
+  
+      InitialPosition = Separator_LastPosition+1;
+      Separator_LastPosition = DataChain.indexOf(SeparatorCharacter, InitialPosition);
+      ArrayIndex =ArrayIndex+1;
+    }
+    Serial.println(ArrayIndex);
+    Serial.println(" ");
+    element = DataChain.substring(InitialPosition, DataChain.length());
+    parameters[ArraySize-1] = element;
+    Serial.println(parameters[0]);
+    Serial.println(parameters[1]);
+    Serial.println(parameters[2]);
+    Serial.println(parameters[3]);
+  }
+  } 
+ 
